7a9522ec7ec8d9fd989d92ebfd65e34364e4e864
Nishchay kumar <phoenixgamerx15@gmail.com>
2025-10-08 02:10:23 +0530

    Add files via upload

---
+++ chess_game/index.js
@@ -0,0 +1,31 @@
+var board2 = Chessboard('board2', {
+    draggable: true,
+    dropOffBoard: 'trash',
+    sparePieces: true,
+    pieceTheme: function (piece) {
+        switch (piece) {
+            case 'wP': return 'chessboardjs-1.0.0/img/chesspieces/wikipedia/wP.png';
+            case 'wR': return 'chessboardjs-1.0.0/img/chesspieces/wikipedia/wR.png';
+            case 'wN': return 'chessboardjs-1.0.0/img/chesspieces/wikipedia/wN.png';
+            case 'wB': return 'chessboardjs-1.0.0/img/chesspieces/wikipedia/wB.png';
+            case 'wQ': return 'chessboardjs-1.0.0/img/chesspieces/wikipedia/wQ.png';
+            case 'wK': return 'chessboardjs-1.0.0/img/chesspieces/wikipedia/wK.png';
+            case 'bP': return 'chessboardjs-1.0.0/img/chesspieces/wikipedia/bP.png';
+            case 'bR': return 'chessboardjs-1.0.0/img/chesspieces/wikipedia/bR.png';
+            case 'bN': return 'chessboardjs-1.0.0/img/chesspieces/wikipedia/bN.png';
+            case 'bB': return 'chessboardjs-1.0.0/img/chesspieces/wikipedia/bB.png';
+            case 'bQ': return 'chessboardjs-1.0.0/img/chesspieces/wikipedia/bQ.png';
+            case 'bK': return 'chessboardjs-1.0.0/img/chesspieces/wikipedia/bK.png';
+            default: return null;
+        }
+    },
+   
+});
+
+$('#startBtn').on('click', function () {
+    board2.start();
+});
+
+$('#clearBtn').on('click', function () {
+    board2.clear();
+});
